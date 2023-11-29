f157acdb274aaa564bf42e65ff10329a4902ce91
Felippe Scheidt <felippescheidt@gmail.com>
2023-11-28 21:43:33 -0300

    update: paises_consultas.py

--- paises_consultas.py
+++ paises_consultas.py
@@ -10,5 +10,77 @@ def consulta_codigo_area(cod):
   pprint(doc)
 consulta_codigo_area("55") # => Brazil
 consulta_codigo_area("595") # => Py
+
+print("="*40)
+print("Paises com fronteira com Brazil:")
+paises = list(
+  db.paises.find(
+    # operador in pesquisa nos valores dentro do vetor
+    { "borders": { "$in": ["BRA"] } },
+    {"name.common":1, "_id":0}
+    )
+)
+print("fazem fronteira:", len(paises))
+pprint(paises)
+
 # rodar no terminal:
 # python3 paises_consultas.py
+
+
+consulta = db.paises.aggregate( 
+[
+  # ESTAGIO 1 
+  { # WHERE
+    "$match": { "region": "Americas" } 
+  }, 
+  # ESTAGIO 2
+  { 
+    "$project": { # SELECT
+        "pais": "$name.common", 
+        "capital": 1, 
+        #"region": "$region",
+        "_id": 0,
+    }
+  },
+  # ESTAGIO 3
+  { "$sort": { "capital": 1 }}
+])
+
+pprint(list(consulta))
+
+
+
+
+
+
+# (12) Países que possuem mais de um idioma oficial:
+q = db.paises.aggregate([
+  { 
+    "$addFields": {
+      "lang_count": {
+        "$size": {
+          "$objectToArray": "$languages"
+        }
+      }
+    }
+  },
+  {
+    "$match": {
+        "lang_count": {"$gte": 5}
+    }
+  },
+  {
+    "$project": {
+        "_id": 0,
+        "country": "$name.common",
+        "langs": "$lang_count",
+        #"languages": 1
+    }
+  }
+])
+res = list(q)
+print(len(res))
+pprint(res)
+
+# Quais são os idiomas oficiais dos paises com mais
+# de 4 idiomas
